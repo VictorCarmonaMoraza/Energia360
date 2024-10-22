06cef6bc254e6dadadf27d8727e671f13cfec7a5
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2024-10-22 20:38:04 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './components/main/welcome/welcome.component';
+import { RegisterComponent } from './components/main/register/register.component';
+import { LoginComponent } from './components/main/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/welcome', pathMatch:'full'},
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/welcome', pathMatch:'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
